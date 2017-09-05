c47d41ca2a4f7a595f083e18b561d35fe40f79f9
zgcmituxiaogaoyang <13540641425@163.com>
2017-09-06 00:45:33 +0800

    my js files

---
+++ script/index.js
@@ -0,0 +1,50 @@
+class PraiseButton {
+    constructor(num, element,url) {
+        this.num = num;
+        this.element = element;
+        this.url=url;
+    }
+
+    clickAction() {
+        this.element.click((e) => {
+            if (this.num < 10) {
+                this.element.css('-webkit-filter', 'grayscale(0)');
+                $('#animation').addClass('num');
+                this.num = add(this.num);
+                setTimeout(function() {
+                    $('#animation').removeClass('num');
+                }, 1000);
+            }else{
+                this.element.css('-webkit-filter', 'grayscale(1)');
+                this.num=0;
+            }
+            console.log(this.num);
+            $.ajax({
+                url:this.url,
+                data:{
+                    num:this.num
+                },
+                type:'get',
+                success:function(data){
+                    console.log(data);
+                },
+                error:function(){
+                    console.log("请求失败！");
+                }
+            });
+        });
+    }
+}
+
+
+class Thumb extends PraiseButton {
+    constructor(num, element,url) {
+        super(num, element,url);
+    }
+}
+export
+default {
+    Thumb
+}
+// let f= new Thumb(0,$('#thumb'));
+// f.clickAction();
